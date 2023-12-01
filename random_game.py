93ceef77488b39560df99ee5b4f29ba4bfaaa2d9
Caleb Butler <butlercq@gmail.com>
2023-12-01 12:00:14 -0500

    Add a file that plays with random inputs

---
+++ random_game.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+# Copyright (c) 2023 Charleston Andrews, Caleb Butler
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+from tetris_game import TetrisGame
+from renderer import Renderer
+import pygame
+import random
+
+
+def main() -> None:
+    game = TetrisGame(60)
+    renderer = Renderer(game)
+    renderer.setup()
+    running = True
+
+    clock = pygame.time.Clock()
+
+    while running:
+        game.step()
+        renderer.rerender()
+        game.set_next_input(random.randint(1, 8))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        if game.is_over():
+            game = TetrisGame(60)
+            renderer.set_game(game)
+        clock.tick(game.get_frame_rate())
+
+
+if __name__ == '__main__':
+    main()
