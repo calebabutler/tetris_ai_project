7f3d9832180dc8853fdfa18f219da9d71367cc70
Caleb Butler <butlercq@gmail.com>
2023-12-09 12:15:12 -0500

    Show all moves by AI, have non-train mode, 60 fps

--- simple_ai.py
+++ simple_ai.py
@@ -26,26 +26,21 @@ import pygame
 import copy
 import numpy as np
 import random
+import sys
 
 
-# Starting weights
-# Currently best weights found so far
-w_1 = 9
-w_2 = 1
-w_3 = 10
-
-score_table = {}
-score_total = 0
-games = 0
-
-
-def move(game: TetrisGame, absolute_position: int, rotation: int) -> None:
+def move(game: TetrisGame, absolute_position: int, rotation: int,
+         renderer: Renderer = None, clock: pygame.time.Clock = None) -> None:
     last_rotation = 100
     current_rotation = game.get_current_piece().rotation
     while (game.get_current_piece().rotation != rotation
            and last_rotation != current_rotation):
         game.set_next_input(Input.C_ROTATE.value)
         game.step()
+        if renderer is not None:
+            renderer.rerender()
+            if clock is not None:
+                clock.tick(game.get_frame_rate())
         last_rotation = current_rotation
         current_rotation = game.get_current_piece().rotation
 
@@ -55,6 +50,10 @@ def move(game: TetrisGame, absolute_position: int, rotation: int) -> None:
            and last_position != current_position):
         game.set_next_input(Input.MOVE_LEFT.value)
         game.step()
+        if renderer is not None:
+            renderer.rerender()
+            if clock is not None:
+                clock.tick(game.get_frame_rate())
         last_position = current_position
         current_position = game.get_current_piece().position[0]
 
@@ -64,6 +63,10 @@ def move(game: TetrisGame, absolute_position: int, rotation: int) -> None:
            and last_position != current_position):
         game.set_next_input(Input.MOVE_RIGHT.value)
         game.step()
+        if renderer is not None:
+            renderer.rerender()
+            if clock is not None:
+                clock.tick(game.get_frame_rate())
         last_position = current_position
         current_position = game.get_current_piece().position[0]
 
@@ -71,7 +74,8 @@ def move(game: TetrisGame, absolute_position: int, rotation: int) -> None:
     game.step()
 
 
-def get_utility(game: TetrisGame, position: int, rotation: int) -> float:
+def get_utility(game: TetrisGame, position: int, rotation: int, w_1: int,
+                w_2: int, w_3: int) -> float:
     old_bumpiness = game.get_bumpiness()
     old_holes = game.get_number_holes()
     old_height = game.get_aggregate_height()
@@ -95,7 +99,8 @@ def get_utility(game: TetrisGame, position: int, rotation: int) -> float:
     return utility
 
 
-def get_next_move(game: TetrisGame) -> (int, int):
+def get_next_move(game: TetrisGame, w_1: int, w_2: int,
+                  w_3: int) -> (int, int):
     next_actions = []
     for i in range(11):
         for j in range(4):
@@ -106,52 +111,59 @@ def get_next_move(game: TetrisGame) -> (int, int):
 
     max_utility = -np.inf
     for action in next_actions:
-        utility = get_utility(game, action[0], action[1])
+        utility = get_utility(game, action[0], action[1], w_1, w_2, w_3)
         if utility > max_utility:
             max_utility = utility
             best_action = action
     return best_action
 
 
-def main() -> None:
-    global w_1, w_2, w_3, games, score_total, score_table
+def main(args: [str]) -> None:
+    # Starting weights
+    # Currently best weights found so far
+    w_1 = 6
+    w_2 = 1
+    w_3 = 1
+
+    if len(args) > 1 and args[1] == 'train':
+        score_table = {}
+        score_total = 0
+        games = 0
+        print((w_1, w_2, w_3))
+
     game = TetrisGame(60)
     renderer = Renderer(game)
     renderer.setup()
     running = True
 
-    rendering_ticks = 0
-    render_frame_rate = 60
-
-    print((w_1, w_2, w_3))
+    clock = pygame.time.Clock()
 
     while running:
-        game.step()
-        if pygame.time.get_ticks() > rendering_ticks:
-            renderer.rerender()
-            rendering_ticks += 1000 // render_frame_rate
-        position, rotation = get_next_move(game)
-        move(game, position, rotation)
+        position, rotation = get_next_move(game, w_1, w_2, w_3)
+        move(game, position, rotation, renderer, clock)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
         if game.is_over():
-            games += 1
-            score_total += game.get_score()
+            if len(args) > 1 and args[1] == 'train':
+                games += 1
+                score_total += game.get_score()
+                if games >= 12:
+                    score_table[(w_1, w_2, w_3)] = score_total
+                    new_weights = max(score_table, key=score_table.get)
+                    while (w_1, w_2, w_3) in score_table:
+                        w_1 = max(1, new_weights[0] + random.randint(-10, 10))
+                        w_2 = max(1, new_weights[1] + random.randint(-10, 10))
+                        w_3 = max(1, new_weights[2] + random.randint(-10, 10))
+                    print((w_1, w_2, w_3))
+                    print(f'Best weights: {new_weights}')
+                    print(
+                        f'With average score: {score_table[new_weights] / 12}')
+                    games = 0
+                    score_total = 0
             game.reset()
-            if games >= 12:
-                score_table[(w_1, w_2, w_3)] = score_total
-                new_weights = max(score_table, key=score_table.get)
-                while (w_1, w_2, w_3) in score_table:
-                    w_1 = max(1, new_weights[0] + random.randint(-10, 10))
-                    w_2 = max(1, new_weights[1] + random.randint(-10, 10))
-                    w_3 = max(1, new_weights[2] + random.randint(-10, 10))
-                print((w_1, w_2, w_3))
-                print(f'Best weights: {new_weights}')
-                print(f'With average score: {score_table[new_weights] / 12}')
-                games = 0
-                score_total = 0
+        clock.tick(game.get_frame_rate())
 
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv)
